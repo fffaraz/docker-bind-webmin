3d2a8722638f546dfab46789ab3e20a1b0f8e2a0
Faraz Fallahi <fffaraz@gmail.com>
2017-01-22 00:18:36 -0600

    webmin

--- script_init.sh
+++ script_init.sh
@@ -3,7 +3,8 @@ set -euxo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get -yq update < /dev/null
-apt-get -yq install apt-show-versions apt-transport-https bind9 bind9-host ca-certificates net-tools unzip wget
+apt-get -yq install apt-transport-https bind9 bind9-host ca-certificates net-tools unzip wget < /dev/null
+apt-get -yq install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python < /dev/null
 
 echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
 wget -qO- http://www.webmin.com/jcameron-key.asc | apt-key add -
