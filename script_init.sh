7973b02b3cf6b93816bda745a3081c3fc9b5c4d0
Faraz Fallahi <fffaraz@gmail.com>
2017-01-22 02:20:34 -0600

    recursion

--- script_init.sh
+++ script_init.sh
@@ -6,8 +6,9 @@ rm -rf /etc/apt/apt.conf.d/docker-gzip-indexes
 
 apt-get -yq update < /dev/null
 apt-get -yq install apt-utils < /dev/null
-apt-get -yq install apt-transport-https bind9 bind9-host ca-certificates net-tools unzip wget < /dev/null
+apt-get -yq install apt-transport-https ca-certificates net-tools unzip wget < /dev/null
 apt-get -yq install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python < /dev/null
+apt-get -yq install bind9 bind9utils bind9-host < /dev/null
 
 echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
 wget -qO- http://www.webmin.com/jcameron-key.asc | apt-key add -
@@ -22,3 +23,14 @@ mkdir -m 0775 -p /var/cache/bind
 chown root:bind /var/cache/bind
 
 rm -rf /var/lib/apt/lists/*
+
+cat > /etc/bind/named.conf.options <<'EOL'
+options {
+	directory "/var/cache/bind";
+	dnssec-validation auto;
+	auth-nxdomain no;    # conform to RFC1035
+	listen-on-v6 { any; };
+	recursion no;
+	allow-transfer { none; };
+}
+EOL
