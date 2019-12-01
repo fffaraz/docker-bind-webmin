efceb19ea12fd41badd1f5ac0403a63894288e8a
Faraz Fallahi <fffaraz@gmail.com>
2019-12-01 15:38:01 -0500

    compose

--- script_init.sh
+++ script_init.sh
@@ -6,12 +6,12 @@ rm -rf /etc/apt/apt.conf.d/docker-gzip-indexes
 
 apt-get -yq update < /dev/null
 apt-get -yq upgrade < /dev/null
-apt-get -yq install apt-utils < /dev/null
+apt-get -yq install apt-utils gnupg2 < /dev/null
 apt-get -yq install apt-transport-https ca-certificates net-tools unzip wget < /dev/null
 apt-get -yq install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python < /dev/null
 apt-get -yq install bind9 bind9utils bind9-host < /dev/null
 
-echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
+echo "deb https://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
 wget -qO- http://www.webmin.com/jcameron-key.asc | apt-key add -
 
 apt-get -yq update < /dev/null
